3d20d966752e3c8c4b75b268c73f79946f5e3583
刘成 <bruceangel@outlook.com>
2016-10-23 17:52:05 +0800

    1

---
+++ Demo_Index/Main.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Demo_Index
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreateIndex1 = new System.Windows.Forms.Button();
+            this.btnCreateIndex2 = new System.Windows.Forms.Button();
+            this.btnSearch1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCreateIndex1
+            // 
+            this.btnCreateIndex1.Location = new System.Drawing.Point(32, 27);
+            this.btnCreateIndex1.Name = "btnCreateIndex1";
+            this.btnCreateIndex1.Size = new System.Drawing.Size(91, 23);
+            this.btnCreateIndex1.TabIndex = 0;
+            this.btnCreateIndex1.Text = "创建索引-单";
+            this.btnCreateIndex1.UseVisualStyleBackColor = true;
+            this.btnCreateIndex1.Click += new System.EventHandler(this.btnCreateIndex_Click);
+            // 
+            // btnCreateIndex2
+            // 
+            this.btnCreateIndex2.Location = new System.Drawing.Point(150, 27);
+            this.btnCreateIndex2.Name = "btnCreateIndex2";
+            this.btnCreateIndex2.Size = new System.Drawing.Size(91, 23);
+            this.btnCreateIndex2.TabIndex = 0;
+            this.btnCreateIndex2.Text = "创建索引-多";
+            this.btnCreateIndex2.UseVisualStyleBackColor = true;
+            this.btnCreateIndex2.Click += new System.EventHandler(this.btnCreateIndex2_Click);
+            // 
+            // btnSearch1
+            // 
+            this.btnSearch1.Location = new System.Drawing.Point(32, 102);
+            this.btnSearch1.Name = "btnSearch1";
+            this.btnSearch1.Size = new System.Drawing.Size(91, 23);
+            this.btnSearch1.TabIndex = 0;
+            this.btnSearch1.Text = "查询1";
+            this.btnSearch1.UseVisualStyleBackColor = true;
+            this.btnSearch1.Click += new System.EventHandler(this.btnSearch1_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 338);
+            this.Controls.Add(this.btnSearch1);
+            this.Controls.Add(this.btnCreateIndex2);
+            this.Controls.Add(this.btnCreateIndex1);
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreateIndex1;
+        private System.Windows.Forms.Button btnCreateIndex2;
+        private System.Windows.Forms.Button btnSearch1;
+    }
+}
+
---
+++ Demo_Index/Main.cs
@@ -0,0 +1,127 @@
+﻿using Elasticsearch.Net;
+using Nest;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo_Index
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 创建索引-单
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCreateIndex_Click(object sender, EventArgs e)
+        {
+            //单node
+            var node = new Uri("http://localhost:9200");
+            var settings = new ConnectionSettings(node);
+            var client = new ElasticClient(settings);
+
+            //插入
+            List<UserInfo> list = new List<UserInfo>();
+            UserInfo info = null;
+            for (int i = 0; i < 100; i++)
+            {
+                info = new UserInfo();
+                info.UserID = i + 1;
+                info.UserName = "笑傲江湖" + i;
+                info.Age = 2 * i;
+                info.AddTime = DateTime.Now;
+                info.Year = 2016;
+                info.IsDeleted = 0;
+                list.Add(info);
+            }
+
+            UserInfo userinfo = new UserInfo();
+            userinfo.UserID = 1;
+            userinfo.UserName = "笑傲江湖";
+            userinfo.Age = 23;
+            userinfo.AddTime = DateTime.Now;
+            userinfo.Year = 2016;
+            userinfo.IsDeleted = 0;
+
+            var result = client.Index(userinfo, o => o.Index("zxxk"));
+           
+        }
+
+        /// <summary>
+        /// 创建索引-多
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCreateIndex2_Click(object sender, EventArgs e)
+        {
+            //多uri
+            var uris = new Uri[] {
+                new Uri("http://localhost:9200")
+            };
+            //多node
+            var nodes = new Node[] {
+                new Node (new Uri("http://localhost:9200")),
+                new Node (new Uri("http://localhost:9200"))
+            };
+
+            #region 连接池类型
+            ////对单节点请求
+            //IConnectionPool pool1 = new SingleNodeConnectionPool(uris.FirstOrDefault());
+
+            ////请求时随机请求各个正常节点，不请求异常节点,异常节点恢复后会重新被请求
+            //IConnectionPool pool2 = new StaticConnectionPool(uris);
+
+            //IConnectionPool pool3 = new SniffingConnectionPool(uris);
+            ////false.创建客户端时，随机选择一个节点作为客户端的请求对象，该节点异常后不会切换其它节点
+            ////true，请求时随机请求各个正常节点，不请求异常节点,但异常节点恢复后不会重新被请求
+            //pool3.SniffedOnStartup = true;
+
+            ////创建客户端时，选择第一个节点作为请求主节点，该节点异常后会切换其它节点，待主节点恢复后会自动切换回来
+            //IConnectionPool pool4 = new StickyConnectionPool(uris);
+            #endregion
+            var pool = new StaticConnectionPool(uris);    //第一种连接池
+            //var pool = new StaticConnectionPool(nodes); //第二种连接池
+                                 
+            var settings = new ConnectionSettings(pool);
+            var client = new ElasticClient(settings);
+        }
+
+        /// <summary>
+        /// 查询1
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSearch1_Click(object sender, EventArgs e)
+        {
+            var node = new Uri("http://localhost:9200");            
+            var settings = new ConnectionSettings(node);
+            var client = new ElasticClient(settings);
+
+            Indices index = "test";
+            //查询
+            var searchResult = client.Search<UserInfo>(s => s.Index(index));
+        }
+    }
+
+    [ElasticsearchType(IdProperty = "UserID")]
+    public class UserInfo
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public int Age { get; set; }
+        public DateTime AddTime { get; set; }
+        public int Year { get; set; }
+        public int IsDeleted { get; set; }
+    }
+}
