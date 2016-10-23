02529b76837ca977459f0f200fc67cbcf6536ba0
刘成 <bruceangel@outlook.com>
2016-10-23 23:13:03 +0800

    3

--- Demo_Index/Main.Designer.cs
+++ Demo_Index/Main.Designer.cs
@@ -31,6 +31,11 @@
             this.btnCreateIndex1 = new System.Windows.Forms.Button();
             this.btnCreateIndex2 = new System.Windows.Forms.Button();
             this.btnSearch1 = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnDeleted1 = new System.Windows.Forms.Button();
+            this.btnDeleted2 = new System.Windows.Forms.Button();
+            this.btnCreateIndex3 = new System.Windows.Forms.Button();
+            this.btnDeleted3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnCreateIndex1
@@ -55,7 +60,7 @@
             // 
             // btnSearch1
             // 
-            this.btnSearch1.Location = new System.Drawing.Point(32, 102);
+            this.btnSearch1.Location = new System.Drawing.Point(32, 121);
             this.btnSearch1.Name = "btnSearch1";
             this.btnSearch1.Size = new System.Drawing.Size(91, 23);
             this.btnSearch1.TabIndex = 0;
@@ -63,17 +68,71 @@
             this.btnSearch1.UseVisualStyleBackColor = true;
             this.btnSearch1.Click += new System.EventHandler(this.btnSearch1_Click);
             // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(32, 173);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(519, 191);
+            this.txtResult.TabIndex = 1;
+            // 
+            // btnDeleted1
+            // 
+            this.btnDeleted1.Location = new System.Drawing.Point(32, 72);
+            this.btnDeleted1.Name = "btnDeleted1";
+            this.btnDeleted1.Size = new System.Drawing.Size(91, 23);
+            this.btnDeleted1.TabIndex = 0;
+            this.btnDeleted1.Text = "删除-范围";
+            this.btnDeleted1.UseVisualStyleBackColor = true;
+            this.btnDeleted1.Click += new System.EventHandler(this.btnDeleted1_Click);
+            // 
+            // btnDeleted2
+            // 
+            this.btnDeleted2.Location = new System.Drawing.Point(150, 72);
+            this.btnDeleted2.Name = "btnDeleted2";
+            this.btnDeleted2.Size = new System.Drawing.Size(91, 23);
+            this.btnDeleted2.TabIndex = 0;
+            this.btnDeleted2.Text = "删除-索引";
+            this.btnDeleted2.UseVisualStyleBackColor = true;
+            this.btnDeleted2.Click += new System.EventHandler(this.btnDeleted2_Click);
+            // 
+            // btnCreateIndex3
+            // 
+            this.btnCreateIndex3.Location = new System.Drawing.Point(261, 27);
+            this.btnCreateIndex3.Name = "btnCreateIndex3";
+            this.btnCreateIndex3.Size = new System.Drawing.Size(91, 23);
+            this.btnCreateIndex3.TabIndex = 0;
+            this.btnCreateIndex3.Text = "只创建索引";
+            this.btnCreateIndex3.UseVisualStyleBackColor = true;
+            this.btnCreateIndex3.Click += new System.EventHandler(this.btnCreateIndex3_Click);
+            // 
+            // btnDeleted3
+            // 
+            this.btnDeleted3.Location = new System.Drawing.Point(261, 72);
+            this.btnDeleted3.Name = "btnDeleted3";
+            this.btnDeleted3.Size = new System.Drawing.Size(91, 23);
+            this.btnDeleted3.TabIndex = 0;
+            this.btnDeleted3.Text = "删除-唯一ID";
+            this.btnDeleted3.UseVisualStyleBackColor = true;
+            this.btnDeleted3.Click += new System.EventHandler(this.btnDeleted3_Click);
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(565, 338);
+            this.ClientSize = new System.Drawing.Size(582, 390);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnDeleted2);
+            this.Controls.Add(this.btnDeleted3);
+            this.Controls.Add(this.btnDeleted1);
             this.Controls.Add(this.btnSearch1);
+            this.Controls.Add(this.btnCreateIndex3);
             this.Controls.Add(this.btnCreateIndex2);
             this.Controls.Add(this.btnCreateIndex1);
             this.Name = "Main";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -82,6 +141,11 @@
         private System.Windows.Forms.Button btnCreateIndex1;
         private System.Windows.Forms.Button btnCreateIndex2;
         private System.Windows.Forms.Button btnSearch1;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnDeleted1;
+        private System.Windows.Forms.Button btnDeleted2;
+        private System.Windows.Forms.Button btnCreateIndex3;
+        private System.Windows.Forms.Button btnDeleted3;
     }
 }
 
--- Demo_Index/Main.cs
+++ Demo_Index/Main.cs
@@ -34,28 +34,23 @@ namespace Demo_Index
             //插入
             List<UserInfo> list = new List<UserInfo>();
             UserInfo info = null;
-            for (int i = 0; i < 100; i++)
+            for (int i = 1; i <= 100; i++)
             {
                 info = new UserInfo();
-                info.UserID = i + 1;
+                info.UserID = i ;
                 info.UserName = "笑傲江湖" + i;
                 info.Age = 2 * i;
                 info.AddTime = DateTime.Now;
                 info.Year = 2016;
                 info.IsDeleted = 0;
-                list.Add(info);
+                list.Add(info);               
             }
 
-            UserInfo userinfo = new UserInfo();
-            userinfo.UserID = 1;
-            userinfo.UserName = "笑傲江湖";
-            userinfo.Age = 23;
-            userinfo.AddTime = DateTime.Now;
-            userinfo.Year = 2016;
-            userinfo.IsDeleted = 0;
+            //client.IndexMany(list, index: "test1");
+            client.IndexMany(list, index: "test2");
+            //client.IndexMany(list,index:"test-index3");
 
-            var result = client.Index(userinfo, o => o.Index("zxxk"));
-           
+            this.txtResult.Text = "[创建索引-单]成功！";
         }
 
         /// <summary>
@@ -91,10 +86,52 @@ namespace Demo_Index
             //IConnectionPool pool4 = new StickyConnectionPool(uris);
             #endregion
             var pool = new StaticConnectionPool(uris);    //第一种连接池
-            //var pool = new StaticConnectionPool(nodes); //第二种连接池
-                                 
+            //var pool = new StaticConnectionPool(nodes); //第二种连接池                                 
             var settings = new ConnectionSettings(pool);
             var client = new ElasticClient(settings);
+
+            UserInfo userinfo = new UserInfo();
+            userinfo.UserID = 0;
+            userinfo.UserName = "笑傲江湖0";
+            userinfo.Age = 6;
+            userinfo.AddTime = DateTime.Now;
+            userinfo.Year = 2016;
+            userinfo.IsDeleted = 0;
+            var result = client.Index(userinfo, o => o.Index("zxxk"));
+
+            this.txtResult.Text = "[创建索引-多]成功！";
+        }
+
+        /// <summary>
+        /// 只创建索引
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCreateIndex3_Click(object sender, EventArgs e)
+        {
+            //单node
+            var node = new Uri("http://localhost:9200");
+            var settings = new ConnectionSettings(node);
+            var client = new ElasticClient(settings);
+
+            //创建1：空索引
+            client.CreateIndex("test1");
+
+            //基本配置
+            IIndexState indexState = new IndexState()
+            {
+                Settings = new IndexSettings()
+                {
+                    NumberOfReplicas = 1,//副本数
+                    NumberOfShards = 5   //分片数
+                }
+            };
+            //创建2：带配置
+            client.CreateIndex("test2", p => p.InitializeUsing(indexState));
+            //创建3：创建并Mapping
+            client.CreateIndex("test-index3", p => p.InitializeUsing(indexState).Mappings(m => m.Map<UserInfo>(mp => mp.AutoMap())));
+
+            this.txtResult.Text = "[只创建索引]成功！";
         }
 
         /// <summary>
@@ -112,16 +149,117 @@ namespace Demo_Index
             //查询
             var searchResult = client.Search<UserInfo>(s => s.Index(index));
         }
+
+        /// <summary>
+        /// 删除-范围
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnDeleted1_Click(object sender, EventArgs e)
+        {
+            //单node
+            var node = new Uri("http://localhost:9200");
+            var settings = new ConnectionSettings(node);
+            //var settings = new ConnectionSettings(node).DefaultIndex("zxxk");//不推荐
+            var client = new ElasticClient(settings);
+
+            Indices indices = "test2";
+            Types types = "userinfo";
+            //批量删除 需要es安装 delete-by-query插件
+            var result = client.DeleteByQuery<UserInfo>(indices, types,
+                dq =>
+                    dq.Query(
+                        q =>
+                            q.TermRange(tr => tr.Field(fd => fd.Age).GreaterThanOrEquals("20").LessThanOrEquals("198")))
+                            );
+            if (result.IsValid)
+            {
+                this.txtResult.Text = "[删除-范围]成功！";
+            }
+            else
+            {
+                this.txtResult.Text = "[删除-范围]失败！";
+            }
+        }
+
+        /// <summary>
+        /// 删除-索引
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnDeleted2_Click(object sender, EventArgs e)
+        {
+            //单node
+            var node = new Uri("http://localhost:9200");
+            var settings = new ConnectionSettings(node);
+            var client = new ElasticClient(settings);
+
+            //索引test是否存在
+            if (client.IndexExists("test").Exists)
+            {
+                client.DeleteIndex("test");
+            }
+
+            client.DeleteIndex("test2");
+
+            this.txtResult.Text = "[删除-索引]成功！";
+        }
+
+        /// <summary>
+        /// 删除-唯一ID
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnDeleted3_Click(object sender, EventArgs e)
+        {
+            //单node
+            var node = new Uri("http://localhost:9200");
+            var settings = new ConnectionSettings(node);
+            var client = new ElasticClient(settings);
+
+            client.Delete<UserInfo>(new UserInfo() { UserID = 100 }, o => o.Index("test2"));
+
+            DocumentPath<UserInfo> deletePath = new DocumentPath<UserInfo>(1);
+            client.Delete(deletePath,o=>o.Index("test1"));
+
+            IDeleteRequest request = new DeleteRequest("test1", "userinfo", 2);
+            client.Delete(request);
+
+            this.txtResult.Text = "[删除-唯一ID]成功！";
+        }
+
+      
     }
 
+    /// <summary>
+    /// 索引唯一Id
+    /// </summary>
     [ElasticsearchType(IdProperty = "UserID")]
     public class UserInfo
     {
+        [Number(NumberType.Long)]
         public int UserID { get; set; }
+        /// <summary>   
+        /// 设置索引时字段的名称
+        /// </summary>
+        [String(Name = "Name")]
         public string UserName { get; set; }
+        [String(Name="Age")]
         public int Age { get; set; }
+        [Date(Format="yyyy-MM-dd")]
         public DateTime AddTime { get; set; }
-        public int Year { get; set; }
+        /// <summary>
+        /// 如果string 类型的字段不需要被分析器拆分，要作为一个正体进行查询，需标记此声明，
+        /// 否则索引的值将被分析器拆分.
+        /// </summary>
+        [String(Index=FieldIndexOption.NotAnalyzed)]
+        public string Description { get; set; }
+        public int Year { get; set; }        
         public int IsDeleted { get; set; }
+        /// <summary>
+        /// 如需使用坐标点类型需添加坐标点特性，在maping时会自动映射类型
+        /// </summary>
+        [GeoPoint(Name = "ZuoBiao", LatLon = true)]
+        public GeoLocation Location { get; set; }
     }
 }
