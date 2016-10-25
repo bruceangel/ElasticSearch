8f14f6226c0282874f2bcc68dcae7f8988c846b1
刘成 <bruceangel@outlook.com>
2016-10-25 23:32:56 +0800

    2

--- Demo_Index/Main.Designer.cs
+++ Demo_Index/Main.Designer.cs
@@ -36,6 +36,9 @@
             this.btnDeleted2 = new System.Windows.Forms.Button();
             this.btnCreateIndex3 = new System.Windows.Forms.Button();
             this.btnDeleted3 = new System.Windows.Forms.Button();
+            this.btnUpdate1 = new System.Windows.Forms.Button();
+            this.btnUpdate2 = new System.Windows.Forms.Button();
+            this.btnGetID1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnCreateIndex1
@@ -60,7 +63,7 @@
             // 
             // btnSearch1
             // 
-            this.btnSearch1.Location = new System.Drawing.Point(32, 121);
+            this.btnSearch1.Location = new System.Drawing.Point(32, 168);
             this.btnSearch1.Name = "btnSearch1";
             this.btnSearch1.Size = new System.Drawing.Size(91, 23);
             this.btnSearch1.TabIndex = 0;
@@ -70,7 +73,7 @@
             // 
             // txtResult
             // 
-            this.txtResult.Location = new System.Drawing.Point(32, 173);
+            this.txtResult.Location = new System.Drawing.Point(32, 219);
             this.txtResult.Multiline = true;
             this.txtResult.Name = "txtResult";
             this.txtResult.Size = new System.Drawing.Size(519, 191);
@@ -116,15 +119,48 @@
             this.btnDeleted3.UseVisualStyleBackColor = true;
             this.btnDeleted3.Click += new System.EventHandler(this.btnDeleted3_Click);
             // 
+            // btnUpdate1
+            // 
+            this.btnUpdate1.Location = new System.Drawing.Point(32, 119);
+            this.btnUpdate1.Name = "btnUpdate1";
+            this.btnUpdate1.Size = new System.Drawing.Size(91, 23);
+            this.btnUpdate1.TabIndex = 0;
+            this.btnUpdate1.Text = "更新-全部";
+            this.btnUpdate1.UseVisualStyleBackColor = true;
+            this.btnUpdate1.Click += new System.EventHandler(this.btnUpdate1_Click);
+            // 
+            // btnUpdate2
+            // 
+            this.btnUpdate2.Location = new System.Drawing.Point(150, 119);
+            this.btnUpdate2.Name = "btnUpdate2";
+            this.btnUpdate2.Size = new System.Drawing.Size(91, 23);
+            this.btnUpdate2.TabIndex = 0;
+            this.btnUpdate2.Text = "更新-部分";
+            this.btnUpdate2.UseVisualStyleBackColor = true;
+            this.btnUpdate2.Click += new System.EventHandler(this.btnUpdate2_Click);
+            // 
+            // btnGetID1
+            // 
+            this.btnGetID1.Location = new System.Drawing.Point(261, 119);
+            this.btnGetID1.Name = "btnGetID1";
+            this.btnGetID1.Size = new System.Drawing.Size(91, 23);
+            this.btnGetID1.TabIndex = 0;
+            this.btnGetID1.Text = "根据唯一ID获取";
+            this.btnGetID1.UseVisualStyleBackColor = true;
+            this.btnGetID1.Click += new System.EventHandler(this.btnGetID1_Click);
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(582, 390);
+            this.ClientSize = new System.Drawing.Size(582, 422);
             this.Controls.Add(this.txtResult);
             this.Controls.Add(this.btnDeleted2);
             this.Controls.Add(this.btnDeleted3);
             this.Controls.Add(this.btnDeleted1);
+            this.Controls.Add(this.btnGetID1);
+            this.Controls.Add(this.btnUpdate2);
+            this.Controls.Add(this.btnUpdate1);
             this.Controls.Add(this.btnSearch1);
             this.Controls.Add(this.btnCreateIndex3);
             this.Controls.Add(this.btnCreateIndex2);
@@ -146,6 +182,9 @@
         private System.Windows.Forms.Button btnDeleted2;
         private System.Windows.Forms.Button btnCreateIndex3;
         private System.Windows.Forms.Button btnDeleted3;
+        private System.Windows.Forms.Button btnUpdate1;
+        private System.Windows.Forms.Button btnUpdate2;
+        private System.Windows.Forms.Button btnGetID1;
     }
 }
 
